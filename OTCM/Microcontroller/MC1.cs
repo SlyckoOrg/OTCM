7c59d6e6465d59cb923796c151f6d1153453ef46
Asa <asareel56@gmail.com>
2022-10-23 11:30:05 -0400

    Added MCG & MC1 attributes and accessors #13 #3

--- OTCM/Microcontroller/MC1.cs
+++ OTCM/Microcontroller/MC1.cs
@@ -2,5 +2,10 @@
 
 public class MC1
 {
-    
+    private List<int> Power { get; set; }
+    private string  Firmware  { get; set; }
+    private string Dd  { get; set; }
+    private  Dictionary<int, string> Gpio  { get; set; }
+    private List<string> Connectors  { get; set; }
+    private List<string> Languages  { get; set; }
 }
--- OTCM/Microcontroller/MCG.cs
+++ OTCM/Microcontroller/MCG.cs
@@ -2,5 +2,13 @@
 
 public class MCG
 {
-    
+    private List<int> Power { get; set; }
+    private double[] Dimensions { get; set; }
+    private string  Manufacturer  { get; set; }
+    private string  Firmware  { get; set; }
+    private string Dd  { get; set; }
+    private  Dictionary<int, string> Gpio  { get; set; }
+    private List<string> Connectors  { get; set; }
+    private bool TestFunction  { get; set; }
+    private List<string> Languages  { get; set; }
 }
