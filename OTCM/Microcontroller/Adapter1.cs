a1f92d4d1ed935f2d0f16556302e9200c4d0548a
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-10-27 15:19:20 -0400

    Create Adapter1.cs #6

---
+++ OTCM/Microcontroller/Adapter1.cs
@@ -0,0 +1,31 @@
+﻿namespace a;
+
+public class Adapter1 : MCG
+{
+    private MC1 _mc1 { get; set; }
+
+    public Adapter1(MC1 mc1)
+    {
+        //Set Dimensions:
+        //weight(g):
+        _dimensions[0] = 100;
+        //lenght(cm):
+        _dimensions[1] = 4.52;
+        //width(cm):
+        _dimensions[0] = 2.09;
+        //thickness(cm):
+        _dimensions[0] = 0.9;
+        
+        //Set Manufacturer:
+        _manufacturer = "Raspberry PI";
+        
+        //Set Firmware:
+        _firmware = "RP4000";
+        
+        //Set Test Function: 
+        _hasTestFunction = true;
+
+        //set address for _mc1:
+        _mc1 = mc1;
+    }
+}
