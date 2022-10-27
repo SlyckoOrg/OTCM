c26bb030c21a351cbb0f6252514553c692bca6a3
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-10-27 14:59:20 -0400

    Update MCG to use protected attributes

--- OTCM/Microcontroller/MCG.cs
+++ OTCM/Microcontroller/MCG.cs
@@ -2,13 +2,13 @@
 
 public class MCG
 {
-    private List<int> Power { get; set; }
-    private double[] Dimensions { get; set; }
-    private string  Manufacturer  { get; set; }
-    private string  Firmware  { get; set; }
-    private string Dd  { get; set; }
-    private  Dictionary<int, string> Gpio  { get; set; }
-    private List<string> Connectors  { get; set; }
-    private bool TestFunction  { get; set; }
-    private List<string> Languages  { get; set; }
+    protected List<int> Power { get; set; }
+    protected double[] Dimensions { get; set; }
+    protected string  Manufacturer  { get; set; }
+    protected string  Firmware  { get; set; }
+    protected string Dd  { get; set; }
+    protected  Dictionary<int, string> Gpio  { get; set; }
+    protected List<string> Connectors  { get; set; }
+    protected bool TestFunction  { get; set; }
+    protected List<string> Languages  { get; set; }
 }
