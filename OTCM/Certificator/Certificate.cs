e5030746e1e88c7dd5515fffda40d025b93ab1e7
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-10-28 12:40:22 -0400

    adding functions for the Certificate class #4

--- OTCM/Certificator/Certificate.cs
+++ OTCM/Certificator/Certificate.cs
@@ -2,5 +2,34 @@
 
 public class Certificate
 {
+    private List<ITest> _tests;
+
+    public Certificate()
+    {
+        _tests = new List<ITest>();
+    }
     
+    public Certificate(List<ITest> tests)
+    {
+        _tests = tests;
+    }
+
+    public bool DoTests(MCG mcg)
+    {
+        for (int i = 0; i < _tests.Count; i++)
+        {
+            // bool testResult = _tests[i].Test(mcg);
+            // if (!testResult)
+            //     return false;
+                //test failed 
+        }
+
+        return true;
+        //test successful
+    }
+
+    public void WriteCertificate()
+    {
+        //Write the certificate
+    }
 }
