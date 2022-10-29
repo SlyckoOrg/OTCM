4e58dd703a4de7a926726db391f17deafbbee44f
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-10-29 16:33:01 -0400

    Adapter 3 #6

--- OTCM/Microcontroller/Adapter3.cs
+++ OTCM/Microcontroller/Adapter3.cs
@@ -6,11 +6,11 @@ public class Adapter3 : MCG
 
     public MC3 _mc3 { get; private set; }
 
-    public Adapter3(List<int> power, double[] dimensions, string manufacturer, string firmware, string dd,
-        Dictionary<int, string> gpio, List<string> connectors, List<string> languages, List<string> ports, MC3 mc3) 
-        : base(power, dimensions, manufacturer, firmware, dd, gpio, connectors, languages, ports)
+    public Adapter3(List<int> power, double[] dimensions, string manufacturer, string firmware, string model, string dd,
+        Dictionary<int, string> gpio, List<string> connectors, bool hasTestFunction, List<string> languages, List<string> ports, MC3 mc3) 
+        : base(power, dimensions, manufacturer, firmware, model, dd, gpio, connectors, hasTestFunction, languages, ports)
     {
-
+        _mc3 = new MC3(power, dimensions, manufacturer, firmware, model, dd, gpio, connectors, hasTestFunction, languages);
     }
 
 }
