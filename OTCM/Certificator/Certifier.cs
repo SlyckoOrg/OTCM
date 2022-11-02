c30b9df2ffdc605c2f2f089abebce514fe3d4c2f
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-02 16:22:57 -0400

    Update Certifier #4

--- OTCM/Certificator/Certifier.cs
+++ OTCM/Certificator/Certifier.cs
@@ -1,6 +1,45 @@
 ﻿namespace a;
 
-public class Certificator
+public class Certifier
 {
+    private List<ITest> _tests;
+    private List<Certificate> _certificates;
+    
+    private List<MCG> _MCGS;
+
+    public void GenerateMCG()
+    {
+        
+    }
+
+    public void GenerateCertificate(Certificate certificate, MCG microcontroller)
+    {
+        
+    }
+
+    public void AddCertificate(Certificate certificate)
+    {
+        
+    }
+
+    public void AddMCG(MCG mcg)
+    {
+        
+    }
+
+    public void SaveMCG(MCG mcg)
+    {
+        
+    }
+
+    public void ReadMCGFile()
+    {
+        
+    }
+
+    public void Dialog()
+    {
+        
+    }
     
 }
