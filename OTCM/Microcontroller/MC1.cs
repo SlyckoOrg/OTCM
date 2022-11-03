65a4d3c4dc6a655d26b97d942c57647a6ddf71dc
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-03 14:51:13 -0400

    Update Default constructor MC1 #3

--- OTCM/Microcontroller/MC1.cs
+++ OTCM/Microcontroller/MC1.cs
@@ -14,9 +14,34 @@ public class MC1
     public MC1()
     {
         _voltage = new List<double>();
+        _voltage.Add(3.3);
+        _firmware = "circuit Python";
+        _dd = "carte SD";
+        
         _gpio = new Dictionary<int, string>();
+        _gpio.Add(1, "VIN");
+        _gpio.Add(2, "DATA");
+        _gpio.Add(3, "DATA");
+        _gpio.Add(4, "DATA");
+        _gpio.Add(5, "DATA");
+        _gpio.Add(6, "DATA");
+        _gpio.Add(7, "OTHER");
+        _gpio.Add(8, "OTHER");
+        _gpio.Add(9, "GND");
+        _gpio.Add(10, "OTHER");
+        _gpio.Add(11, "OTHER");
+        _gpio.Add(12, "DATA");
+        _gpio.Add(13, "DATA");
+        _gpio.Add(14, "OTHER");
+        _gpio.Add(15, "OTHER");
+        _gpio.Add(16, "OTHER");
+        
         _connectors = new List<string>();
+        _connectors.Add("HDMI");
+        _connectors.Add("micro-USB");
+        _connectors.Add("WIFI");
         _languages = new List<string>();
+        _languages.Add("Python");
     }
 
     public MC1( List<double> power, string firmware,string dd, Dictionary<int, string> gpio, List<string> connectors, List<string> languages)
