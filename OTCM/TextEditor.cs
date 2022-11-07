e603a5b241ee9721de7ae17dd5b5a8f4e4f2f9d6
Asaréel Dadiouari <asareel56@gmail.com>
2022-11-06 20:54:20 -0500

    refactor WriteText function

--- OTCM/TextEditor.cs
+++ OTCM/TextEditor.cs
@@ -1,36 +1,19 @@
 ﻿namespace OTCM;
 public class TextEditor
 {
-        // public static async Task WriteText(string fileName, string[] lines)
-        // {
-        //     Console.WriteLine("writing ...");
-        //     await File.WriteAllLinesAsync(fileName, lines);
-        // }
-
         public void WriteText(string fileName, string[] lines)
         {
                 string projectRootPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-                string[] folderPaths = Directory.GetDirectories(@$"{projectRootPath}", "*.*",
-                        SearchOption.TopDirectoryOnly);
                 string resultFolderPath = projectRootPath + "//Results";
                 StreamWriter writer = null;
 
                 try
                 {
-                        if (!folderPaths.Contains("Results"))
-                        {
-                                writer = new StreamWriter(Directory.CreateDirectory(resultFolderPath).FullName + "//certificat.txt");
-                        }
-                        else
-                        {
-                                var file = File.Create(resultFolderPath + $"//{fileName}");
-                                writer = new StreamWriter(file);
-                        }
+                        writer = new StreamWriter(File.Create(resultFolderPath + $"//{fileName}"));
                 }
                 catch (Exception e)
                 {
-                        Console.WriteLine(e);
-                        throw;
+                        writer = new StreamWriter(Directory.CreateDirectory(resultFolderPath).FullName + $"//{fileName}");
                 }
                 finally
                 {
