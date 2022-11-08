87b6f65e503368d394145c7956af83f11f3120a6
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-08 12:23:55 -0500

    Update MCG and Adapters Attributes and constructors
    update according to CON V3

--- OTCM/Microcontroller/Adapter2.cs
+++ OTCM/Microcontroller/Adapter2.cs
@@ -4,9 +4,26 @@ public class Adapter2 : MCG
 {
     public MC2 _mc2 { get; private set; }
 
-    public Adapter2(List<double> voltage, double[] dimensions, string manufacturer, string firmware, string model, string dd,
-        Dictionary<int, string> gpio, List<string> connectors, bool hasTestFunction, List<string> languages, List<string> ports, MC2 mc2) 
-        : base(voltage, dimensions, manufacturer, firmware, model, dd, gpio, connectors, hasTestFunction, languages, ports)
+    public Adapter2(List<double> voltage, double[] dimensions, string producer, string firmware, string model, string disk,
+        Dictionary<int, string> gpios, List<string> ports, bool hasTestFunction, List<string> languages, MC2 mc2) 
+        : base(voltage, dimensions, producer, firmware, model, disk, gpios, ports, hasTestFunction, languages)
+    {
+        //Set Firmware:
+        _firmware = "Inconnu";
+        
+        //Set ports:
+        _ports = new List<string> { "HDMI", "USB", "WIFI", "Bluetooth"};
+        
+        //Set languages:
+        _languages = new List<string> {"C++" };
+
+        _mc2 = mc2;
+        
+        //thickness(cm):
+        _mc2._dimensions[3] = 0.77;
+    }
+
+    public Adapter2(MC2 mc2)
     {
         //Set Firmware:
         _firmware = "Inconnu";
