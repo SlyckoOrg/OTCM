3cf55917ee85e3f465ef3ed5fa127e9d268e088f
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-08 13:01:26 -0500

    Update MC2 property name #23

--- OTCM/Microcontroller/MC2.cs
+++ OTCM/Microcontroller/MC2.cs
@@ -8,7 +8,7 @@ public class MC2
     public string  _producer  { get; private set; }
     public string _model { get; private set; }
     public string _disk  { get; private set; }
-    public  Dictionary<int, string> _gpio  { get; private set; }
+    public  Dictionary<int, string> _gpios  { get; private set; }
     public bool _isTestSystem  { get; private set; }
 
     public MC2()
@@ -21,40 +21,40 @@ public class MC2
         _model = "RP2000";
         _disk = "Intégré";
         
-        _gpio = new Dictionary<int, string>();
-        _gpio.Add(1, "DATA");
-        _gpio.Add(2, "DATA");
-        _gpio.Add(3, "DATA");
-        _gpio.Add(4, "DATA");
-        _gpio.Add(5, "DATA");
-        _gpio.Add(6, "DATA");
-        _gpio.Add(7, "DATA");
-        _gpio.Add(8, "DATA");
-        _gpio.Add(9, "VIN");
-        _gpio.Add(10, "VIN");
-        _gpio.Add(11, "DATA");
-        _gpio.Add(12, "DATA");
-        _gpio.Add(13, "OTHER");
-        _gpio.Add(14, "OTHER");
-        _gpio.Add(15, "OTHER");
-        _gpio.Add(16, "OTHER");
-        _gpio.Add(17, "OTHER");
-        _gpio.Add(18, "OTHER");
-        _gpio.Add(19, "GND");
-        _gpio.Add(20, "GND");
+        _gpios = new Dictionary<int, string>();
+        _gpios.Add(1, "DATA");
+        _gpios.Add(2, "DATA");
+        _gpios.Add(3, "DATA");
+        _gpios.Add(4, "DATA");
+        _gpios.Add(5, "DATA");
+        _gpios.Add(6, "DATA");
+        _gpios.Add(7, "DATA");
+        _gpios.Add(8, "DATA");
+        _gpios.Add(9, "VIN");
+        _gpios.Add(10, "VIN");
+        _gpios.Add(11, "DATA");
+        _gpios.Add(12, "DATA");
+        _gpios.Add(13, "OTHER");
+        _gpios.Add(14, "OTHER");
+        _gpios.Add(15, "OTHER");
+        _gpios.Add(16, "OTHER");
+        _gpios.Add(17, "OTHER");
+        _gpios.Add(18, "OTHER");
+        _gpios.Add(19, "GND");
+        _gpios.Add(20, "GND");
 
         _isTestSystem = true;
     }
 
     public MC2(List<double> voltage, double[] dimensions, string producer, string model, string disk,
-        Dictionary<int, string> gpio, bool isTestSystem)
+        Dictionary<int, string> gpios, bool isTestSystem)
     {
         _voltage = voltage;
         _dimensions = dimensions;
         _producer = producer;
         _model = model;
         _disk = disk;
-        _gpio = gpio;
+        _gpios = gpios;
         _isTestSystem = isTestSystem;
     }
 }
