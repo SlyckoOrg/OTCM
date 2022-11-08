ee9ea58d3feab86a87efff61d3adf1727030cc72
Maxime COSIALLS <79311075+Maxjuco@users.noreply.github.com>
2022-11-08 13:07:59 -0500

    Remove _mc3 property from Adapter3 #23

--- OTCM/Microcontroller/Adapter3.cs
+++ OTCM/Microcontroller/Adapter3.cs
@@ -4,18 +4,34 @@ public class Adapter3 : MCG
 
 {
 
-    public MC3 _mc3 { get; private set; }
-
     public Adapter3(List<double> voltage, double[] dimensions, string producer, string firmware, string model, string disk,
-        Dictionary<int, string> gpios, List<string> ports, bool hasTestFunction, List<string> languages, MC3 mc3) 
-        : base(voltage, dimensions, producer, firmware, model, disk, gpios, ports, hasTestFunction, languages)
+        Dictionary<int, string> gpios, List<string> ports, bool isTestSystem, List<string> languages, MC3 mc3) 
+        : base(voltage, dimensions, producer, firmware, model, disk, gpios, ports, isTestSystem, languages)
     {
-        _mc3 = mc3;
+        _voltage = voltage;
+        _dimensions = dimensions;
+        _producer = producer;
+        _model = model;
+        _firmware = firmware;
+        _disk = disk;
+        _gpios = gpios;
+        _ports = ports;
+        _isTestSystem = isTestSystem;
+        _languages = languages;
     }
 
     public Adapter3(MC3 mc3)
     {
-        _mc3 = mc3;
+        _voltage = mc3._voltage;
+        _dimensions = mc3._dimensions;
+        _producer = mc3._producer;
+        _model = mc3._model;
+        _firmware = mc3._firmware;
+        _disk = mc3._disk;
+        _gpios = mc3._gpios;
+        _ports = mc3._ports;
+        _isTestSystem = mc3._isTestSystem;
+        _languages = mc3._languages;
     }
 
 }
