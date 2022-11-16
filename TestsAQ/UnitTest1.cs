808baa200121e59ef084988c49428ea6ca5d6def
Pholith <vincent22062@gmail.com>
2022-11-16 16:37:25 -0500

    Création du projet de test

---
+++ TestsAQ/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsAQ
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            
+        }
+    }
+}
