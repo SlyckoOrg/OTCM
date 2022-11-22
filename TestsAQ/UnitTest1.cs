78f65beeb579e301587a7b359d7260a2fe7708e5
lois deniau <deniaulois@gmail.com>
2022-11-22 17:18:12 -0500

    execute test that start app and write 1 1 1 2 in it. the test do start the app, do select first mc, do start mctest, but crash when the app try to redifine output cursor when testing the mc

--- TestsAQ/UnitTest1.cs
+++ TestsAQ/UnitTest1.cs
@@ -1,20 +1,56 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OCTM;
+using OTCM.Interface;
 using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace TestsAQ
 {
     [TestClass]
     public class Tests
     {
-        [TestMethod]
+        public static void TestApp()
+        {
+            Console.WriteLine("Starting app");
+            Interface main = new Interface();
+
+            main.Run();
+            Console.WriteLine("app execution finished");
+        }
+
+    [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(false);
-            
-            
-            MainClass.Main(null);
 
+            //arrange
+
+            Interface main = new Interface();
+            var fakeInput =     "1\n" +
+                                "1\n" +
+                                "1\n" +
+                                "2\n";
+            var outputobjective = ""; //TODO fill this
+
+            var stringReader = new StringReader(fakeInput);
+            Console.SetIn(stringReader);
+
+            //we will use this to catch the output
+            //var stringWriter = new StringWriter();
+            //Console.SetOut(stringWriter);
+
+            //act
+
+            main.Run();
+
+            Assert.IsTrue(true);
+
+            //assert
+
+            //we will use this to check if the output is what we expect
+            //var output = stringWriter.ToString();
+            //Assert.AreEqual(outputobjective, output);
         }
     }
 }
