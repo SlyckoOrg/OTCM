2f8cd7bb20c96935a78ba94899f7870b4a748e1b
lois deniau <deniaulois@gmail.com>
2022-11-23 11:54:34 -0500

    assert output

--- TestsAQ/UnitTest1.cs
+++ TestsAQ/UnitTest1.cs
@@ -15,19 +15,17 @@ namespace TestsAQ
         {
             //arrange
             Interface main = new Interface();
+
             var stringReader = new StringReader(fakeInput);
             Console.SetIn(stringReader);
-
-            //we will use this to catch the output
-            //var stringWriter = new StringWriter();
-            //Console.SetOut(stringWriter);
+ 
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
 
             //act
             main.Run();
-
             //return result
-            return null;
-            //return stringWriter.ToString();
+            return stringWriter.ToString();
         }
 
     [TestMethod]
@@ -38,16 +36,14 @@ namespace TestsAQ
                                 "1\n" +
                                 "1\n" +
                                 "2\n";
-            var outputobjective = ""; //TODO fill this
-
+            var outputobjective = "\u001b[1;34m       ___                         ___           ___     \r\n      /\\  \\                       /\\__\\         /\\  \\ \r\n     /::\\  \\         ___         /:/  /        |::\\  \\   \r\n    /:/\\:\\  \\       /\\__\\       /:/  /         |:|:\\  \\  \r\n   /:/  \\:\\  \\     /:/  /      /:/  /  ___   __|:|\\:\\  \\ \r\n  /:/__/ \\:\\__\\   /:/__/      /:/__/  /\\__\\ /::::|_\\:\\__\\ \r\n  \\:\\  \\ /:/  /  /::\\  \\      \\:\\  \\ /:/  / \\:\\~~\\  \\/__/\r\n   \\:\\  /:/  /  /:/\\:\\  \\      \\:\\  /:/  /   \\:\\  \\      \r\n    \\:\\/:/  /   \\/__\\:\\  \\      \\:\\/:/  /     \\:\\  \\     \r\n     \\::/  /         \\:\\__\\      \\::/  /       \\:\\__\\    \r\n      \\/__/           \\/__/       \\/__/         \\/__/    \r\n  Outil de Test et de Certification pour Microcontrôleurs\r\n\u001b[0m\r\n\u001b[1;94m[OTCM] \u001b[0mVeuillez sélectionner un mode\r\n\u001b[1;32m    1- Mode démonstration    2- Mode expérience    3- Resauvegarder les microcontrôleurs    4- Afficher les microcontrôleurs\r\n\u001b[1;0m\r\n\u001b[1;94m[OTCM] \u001b[0mVeuillez choisir le microcontrôleur à utiliser\r\n\u001b[1;32m    1- MC1    2- MC2    3- MC3\u001b[1;0m\r\n\u001b[1;94m[OTCM] \u001b[0mVeuillez choisir un certificat\r\n\u001b[1;32m    1- Certificat #1    2- Certificat #2    3- Certificat #3\u001b[1;0m\r\n\u001b[1;94m[OTCM] \u001b[0m\u001b[1;34mLancement des tests\u001b[1;0m\r\n\u001b[1;94m[OTCM] \u001b[0m\u001b[1;33mÉchec de la certification\u001b[1;0m\r\n\u001b[1;94m[OTCM] \u001b[0mVeuillez effectuer un choix parmis les options suivantes\r\n\u001b[1;32m    1- Relancer le programme    2- Quitter le programme\u001b[1;0m"; //TODO fill this
+            
 
             // act
             var output = TestApp(fakeInput);
             
             // assert
-            Assert.IsTrue(true);
-            //we will use this to check if the output is what we expect
-            //Assert.AreEqual(outputobjective, output);
+            Assert.AreEqual(outputobjective, output);
         }
     }
 }
